7c2e27f3da5848592c92dd9843efffa325cf55d7
Dai Yufeng <daiyufeng@Dais-MacBook-Pro.local>
2022-12-02 13:44:02 +0800

    add other receipt format

--- routers/ocr_tools/receipt_ocr.py
+++ routers/ocr_tools/receipt_ocr.py
@@ -1,11 +1,13 @@
 import fitz
 import cv2
 import numpy as np
+import re
 from commons.logger import logger as logging
 from paddleocr import PaddleOCR
 # from pytesseract import Output
 # import pytesseract
 from collections import defaultdict
+from PIL import Image
 
 
 
@@ -19,13 +21,19 @@ class receipt():
     def chose_file(self):
         if self.filename.split('.')[-1] == 'pdf':
             doc = fitz.open(stream=self.content,filetype='pdf')
-            page = doc.load_page(0)
+            page = doc[0]
             text = page.get_text('text')
             if len(text)>20:
                 output = self.process_rawpdf(doc)
                 return output
             else:
-                output = self.process_image(doc)
+                mat = fitz.Matrix(2,2)
+                pm = page.get_pixmap(matrix=mat,alpha=False)
+                if pm.width > 2000 or pm.height > 2000:
+                    pm = page.get_pixmap(matrix=fitz.Matrix(1,1),alpha=False)
+                img = Image.frombytes("RGB",[pm.width,pm.height],pm.samples)
+                img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+                output = self.process_image(img)
                 return output
         else:
             image = cv2.imdecode(np.fromstring(self.content, np.uint8), cv2.IMREAD_UNCHANGED)
@@ -43,9 +51,9 @@ class receipt():
 
     def process_image(self,image):
         result = self.model.ocr(image)
-        for i in result[0]:
-            print(i)
-        return result[0]
+        text_content = ' '.join([i[1][0] for i in result[0]])
+        output = self.extract_bluebird(result)
+        return output
 
 
     def extract_grab(self,doc):
@@ -81,10 +89,44 @@ class receipt():
         page_0 = doc.load_page(0)
         text_0 = page_0.get_text('text')
         text_lis0 = text_0.split('\n')
+        info_dict = {}
+        for i,j in enumerate(text_lis0):
+            if 'Total paid' in j:
+                info_dict['fare'] = text_lis0[i+1]
+            elif 'Trip details' in j:
+                info_dict['name'] = text_lis0[i+1]
+            elif 'Distance' in j:
+                info_dict['distance'] = ''.join(text_lis0[i].split(' ')[-2:])
+            elif 'Picked up' in j:
+                info_dict['start time'] = text_lis0[i].split(' ')[-2]
+                info_dict['start place'] = text_lis0[i+1]
+            elif 'Arrived at' in j:
+                info_dict['end time'] = text_lis0[i].split(' ')[-2]
+                info_dict['end place'] = text_lis0[i+1]
  
 
-        return text_lis0
+        return info_dict
 
+
+    def extract_bluebird(self,result):
+        recep_info = {}
+        time_list = []
+        date_list = []
+      
+        text = ' '.join([i[1][0] for i in result[0]])
+        time_list = re.findall(r'\d\d:\d\d',text)
+        date_list = re.findall(r'\d\d/\d\d/\d\d\d\d',text)
+        recep_info['start_time'],recep_info['end_time'] = time_list[0],time_list[1]
+        recep_info['date']=date_list[0]
+
+        for i in result:
+            if 'Km' in i[1][0]:
+                recep_info['Distance'] = i[1][0]
+        
+        return recep_info
+            
+                
+        
 
 
 
